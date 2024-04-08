7172648a1c4b1d99731d8f0f01e7b3a01ee16ac5
Elena Daurtseva <diosama99@gmail.com>
2024-04-09 01:20:42 +0700

    add pages for each title

--- app4-PDF/main.py
+++ app4-PDF/main.py
@@ -6,11 +6,14 @@ pdf = FPDF(orientation="P", unit="mm", format="A4")
 df = pd.read_csv("003 topics.csv")
 
 for index, row in df.iterrows():
+
     pdf.add_page()
     pdf.set_font(family="Times", style="B")
     pdf.set_text_color(100, 100, 100)
     pdf.cell(w=0, h=12, txt=row["Topic"], align='L', ln=1)
     pdf.line(10, 20, 200, 20)
 
+    for i in range(row["Pages"]-1):
+        pdf.add_page()
 
 pdf.output("output.pdf")
